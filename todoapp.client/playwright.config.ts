bd68cb773fe9aafbf9af88401c28cd517beaa45d
sara-lindstrom <sara.maria.lindstrom@hotmail.com>
2025-12-22 18:17:44 +0100

    update working directory in yml file

--- todoapp.client/playwright.config.ts
+++ todoapp.client/playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests-e2e',
+    testDir: './todoapp.client/tests-e2e',
   testMatch: '*.spec.ts',
   /* Run tests in files in parallel */
   fullyParallel: true,
